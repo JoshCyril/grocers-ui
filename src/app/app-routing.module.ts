edae49271c2a3c02afc2a36faafae7416aecfc56
JoshCyril <joshua.cyril88@gmail.com>
2022-11-09 18:48:25 +0530

    api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './comp/home/home.component';
+import { SigninComponent } from './comp/signin/signin.component';
+import { SignupComponent } from './comp/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: 'Home', pathMatch: 'full' },
+  { path: "Home", component: HomeComponent },
+  { path: "Login", component: SigninComponent },
+  { path: "Signin", component: SigninComponent },
+  { path: "Signup", component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,75 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { User } from '../models/user.model';
+import { UserDB } from '../models/userDB.model';
+import { Category } from '../models/category.model';
+import { Product } from '../models/product.model';
+import { Order } from '../models/order.model';
+import { Wishlist } from '../models/wishlist.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  baseUrl: string = "http://localhost:3000/api/";
+  dbUser: string = "user";
+  dbProduct: string = "product";
+  dbCategory: string = "category";
+  dbOrder: string = "order";
+  dbWishlist: string = "wishlist";
+
+  // ! Users --------------
+  loginUser(name: string, email: string, password: string): Observable<User> {
+    return this.http.get<User>(this.baseUrl + "login" + "?name=" + name + "&email=" + email + "&password=" + password);
+  }
+  chkUserExist(): Observable<UserDB[]> {
+    return this.http.get<UserDB[]>(this.baseUrl + "reg");
+  }
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + "all" + "?_db=" + this.dbUser);
+  }
+  getUserById(id: string): Observable<User> {
+    return this.http.get<User>(this.baseUrl + id);
+  }
+  removeUserById(id: string) {
+    return this.http.delete(this.baseUrl + "remove/" + id + "?_db=" + this.dbUser);
+  }
+  addUser(user: User) {
+    return this.http.post(this.baseUrl + "add/user", user);
+  }
+  modifyUser(user: User, id: string) {
+    return this.http.put(this.baseUrl + "modify/" + id + "?_db=" + this.dbUser, user);
+  }
+
+
+  // ! Category --------------
+  getAllCategory(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.baseUrl + "all" + "?_db=" + this.dbProduct);
+  }
+
+
+  // ! Products --------------
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl + "all" + "?_db=" + this.dbProduct);
+  }
+
+
+  // ! Orders --------------
+  getAllOrdersById(id: string): Observable<Order[]> {
+    return this.http.get<Order[]>(this.baseUrl + "order" + "?_db=" + this.dbOrder + "&id=" + id);
+  }
+
+
+  // ! WishList --------------
+  getAllWishListById(id: string): Observable<Wishlist[]> {
+    return this.http.get<Wishlist[]>(this.baseUrl + "wishlist" + "?_db=" + this.dbOrder + "&id=" + id);
+  }
+
 }
